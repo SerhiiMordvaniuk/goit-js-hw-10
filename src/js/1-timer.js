ce18b16c8a65e47db8ff51dd784b5a0bbd50148a
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-23 19:19:40 +0200

    task 1

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,6 +6,7 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
+
 let userSelectedDate = null;
 
 const options = {
@@ -14,11 +15,9 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        
         if (options.defaultDate >= selectedDates[0]) {
-            startBtn.style.backgroundColor = "#cfcfcf";
-            startBtn.style.color = "#989898";
-            startBtn.style.pointerEvents = "none";
+            inactiveStartBtn()
             iziToast.error({
                 message: 'Please choose a date in the future',
                 position: 'topCenter',
@@ -33,19 +32,77 @@ const options = {
             startBtn.style.backgroundColor = "#4e75ff";
             startBtn.style.color = "#fff";
             startBtn.style.pointerEvents = "auto";
+            userSelectedDate = selectedDates[0];
+            startBtn.disabled = false
             }
     },
+    
 };
-
 
 flatpickr("#datetime-picker", options);
 
+
+
 const startBtn = document.querySelector("button[data-start]");
+startBtn.addEventListener("click", timer)
+const input = document.querySelector('.input');
 
 
-startBtn.addEventListener("click", () => {
-    console.log("no");
+function inactiveStartBtn() {
+    startBtn.style.backgroundColor = "#cfcfcf";
+    startBtn.style.color = "#989898";
+    startBtn.style.pointerEvents = "none";
+}
+
+
+let intervalId = null;
+
+function timer() {
+    inactiveStartBtn()
+    input.disabled = true;  
+    let endTimer = userSelectedDate.getTime();
+    let startTimer = options.defaultDate.getTime()
+    let stopTimer = endTimer - startTimer;
+    intervalId = setInterval(() => {
+        stopTimer -= 1000
+        convertMs(stopTimer);
+        
+        if (stopTimer < 1000) {
+        iziToast.error({
+            message: 'Timer has expired ',
+            position: 'topCenter',
+            color: 'green',
+            messageColor: "black",
+            close: false,
+            timeout: 4000,
+            progressBar: false
+        });
+            clearInterval(intervalId)
+        }
+    }, 1000);
     
-})
+}
+
+const spanDays = document.querySelector('[data-days]');
+const spanHours = document.querySelector('[data-hours]');
+const spanMinutes = document.querySelector('[data-minutes]');
+const spanSeconds = document.querySelector('[data-seconds]');
 
 
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day).toString().padStart(2,'0');
+    const hours = Math.floor((ms % day) / hour).toString().padStart(2,'0');
+    const minutes = Math.floor(((ms % day) % hour) / minute).toString().padStart(2,'0');
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second).toString().padStart(2,'0');
+
+    spanDays.textContent = `${days}`;
+    spanHours.textContent = `${hours}`;
+    spanMinutes.textContent = `${minutes}`;
+    spanSeconds.textContent = `${seconds}`;
+}
