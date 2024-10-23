93d0d396433047934680be3fcd854084d6e3dd99
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-23 12:29:34 +0200

    add html

---
+++ src/js/1-timer.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const startBtn = document.querySelector("button[data-start]");
+
+console.log(startBtn);
+
+startBtn.addEventListener("click", () => {
+    startBtn.style.backgroundColor = "#cfcfcf";
+    startBtn.style.color = "#989898";
+    startBtn.style.pointerEvents = "none";
+})
