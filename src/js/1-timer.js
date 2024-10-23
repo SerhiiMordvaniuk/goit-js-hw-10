b8006f0fc07654d8e8ef1430fee2078014dd4198
Serhii Mordvaniuk <sm636332@gmail.com>
2024-10-23 16:03:45 +0200

    a

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,40 +1,51 @@
 "use strict"
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 
 let userSelectedDate = null;
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-      userSelectedDate = selectedDates;
-      console.log(userSelectedDate);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        if (options.defaultDate >= selectedDates[0]) {
+            startBtn.style.backgroundColor = "#cfcfcf";
+            startBtn.style.color = "#989898";
+            startBtn.style.pointerEvents = "none";
+            iziToast.error({
+                message: 'Please choose a date in the future',
+                position: 'topCenter',
+                color: 'red',
+                messageColor: "black",
+                close: false,
+                timeout: 4000,
+                progressBar: false
+            });
+        }
+        else {
+            startBtn.style.backgroundColor = "#4e75ff";
+            startBtn.style.color = "#fff";
+            startBtn.style.pointerEvents = "auto";
+            }
+    },
 };
-
-console.log(userSelectedDate);
-
 
 
 flatpickr("#datetime-picker", options);
 
 const startBtn = document.querySelector("button[data-start]");
 
-// console.log(startBtn);
 
-// startBtn.addEventListener("click", () => {
-//     startBtn.style.backgroundColor = "#cfcfcf";
-//     startBtn.style.color = "#989898";
-//     startBtn.style.pointerEvents = "none";
-// })
+startBtn.addEventListener("click", () => {
+    console.log("no");
+    
+})
 
 
-
-
-
- 
